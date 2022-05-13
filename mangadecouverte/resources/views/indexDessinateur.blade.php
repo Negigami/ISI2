03869823ee41abb00ea865228916dc3b4d579530
Negigami <clementbouchar@gmail.com>
2022-05-13 16:13:02 +0200

    section 5

--- mangadecouverte/resources/views/indexDessinateur.blade.php
+++ mangadecouverte/resources/views/indexDessinateur.blade.php
@@ -1,14 +1,26 @@
-<table class="table table-bordered table-striped">
-    <thead>
-        <th>Id</th>
-        <th>Nom</th>
-        <th>Prénom</th>
-    </thead>
-    @foreach($dessinateurs as $dessinateur)
-        <tr>
-            <td> {{ $dessinateur->id_dessinateur }} </td>
-            <td> {{ $dessinateur->nom_dessinateur }} </td>
-            <td> {{ $dessinateur->prenom_dessinateur }} </td>
-        </tr>
-    @endforeach
-</table>
+@extends('layouts.layout')
+
+@section('titrePage')
+    Listes des dessinnateurs
+@endsection
+
+@section('titreItem')
+    <h1>Tous les dessinateurs :</h1>
+@endsection
+
+@section('contenu')
+    <table class="table table-bordered table-striped">
+        <thead>
+            <th>Id</th>
+            <th>Nom Dessinateur</th>
+            <th>Prénom Dessinateur</th>
+        </thead>
+        @foreach($dessinateurs as $dessinateur)
+            <tr>
+                <td> {{ $dessinateur->id_dessinateur }} </td>
+                <td> {{ $dessinateur->nom_dessinateur }} </td>
+                <td> {{ $dessinateur->prenom_dessinateur }} </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
